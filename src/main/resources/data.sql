dfc8c22960634c6d666abbaad18e2f04041b2226
Anna Khramova <xaa2128@mail.ru>
2024-09-18 18:08:56 +0300

    Add jdbc

---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+insert into netology.customers (name, surname, age, phone_number) values
+                                                                      ('alexey', 'petrov', 23, 89534562178),
+                                                                      ('anna', 'petrova', 27, 89534513247),
+                                                                      ('alexey', 'ivanov', 20, 89539877670),
+                                                                      ('ivan', 'petrov', 25, 89534509876),
+                                                                      ('inna', 'kotova', 24, 89530987651);
+
+insert into netology.orders (date, customer_id, product_name, amount) values
+                                                                          ('2024-03-14', 1, 'laptop', 175000),
+                                                                          ('2024-03-14', 2, 'laptop', 168000),
+                                                                          ('2024-03-14', 1, 'desktop', 275000),
+                                                                          ('2024-03-14', 3, 'mouse', 15000),
+                                                                          ('2024-03-14', 2, 'desktop', 230000),
+                                                                          ('2024-03-14', 4, 'keyboard', 17000),
+                                                                          ('2024-03-14', 5, 'keyboard', 15000);
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,22 @@
+drop table netology.orders;
+
+drop table netology.customers;
+
+create table if not exists netology.customers
+(
+    id bigserial primary key,
+    name text not null,
+    surname text not null,
+    age int not null,
+    phone_number bigint not null
+);
+
+create table if not exists netology.orders
+(
+    id bigserial primary key,
+    date date not null,
+    customer_id bigserial not null,
+    product_name text not null,
+    amount bigint not null,
+    constraint customer_order_fk foreign key (customer_id) references netology.customers (id)
+);
